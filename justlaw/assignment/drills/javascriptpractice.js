922ac5535981481c85337a83e7084ff01f3c3097
olatunji-20 <Falanasheriffdeen@gmail.com>
2021-05-09 13:10:07 +0100

    trying to host my portfolio page

---
+++ justlaw/assignment/drills/javascriptpractice.js
@@ -0,0 +1,20 @@
+
+document.getElementById("sheriff").innerHTML = "hakjfnjeen"
+
+window.alert("welcome to COVID-19 era")
+var customer = prompt("what is your name?")
+alert(customer.length);
+
+var x = prompt("how many bottles of groundnuts do you want" + " " + customer + "?");
+var y = 50;
+
+var z = x * y;
+alert("your total cost of groundnut is" + " " + z)
+
+function adisa(o, p) {
+    return (o * p);
+}
+
+confirm("are you a boy?")
+
+document.getElementById("chelsea").innerHTML = adisa(2, 3);
